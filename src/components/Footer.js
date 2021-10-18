191092c77bff97645606a98539d9106f00e7fbce
bladeyeight <samueljperry1991@gmail.com>
2021-10-18 12:22:05 -0400

    edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 function Footer(props) {
     return (
     <div>
-    <h1>Footer</h1>
     <p>phone:(804)3565005</p>
     <p>linkedin:www.linkedin.com/in/samueljamesperry</p>
     <p>github:https://github.com/bladeyeight</p>
