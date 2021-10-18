b9ef2307e224b849e201d384f670e15fcc59b820
bladeyeight <samueljperry1991@gmail.com>
2021-10-18 12:26:18 -0400

    edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 function Footer(props) {
     return (
     <div>
+    <p>Email:samueljperry1991@gmail.com</p>
     <p>phone:(804)3565005</p>
-    <p>linkedin:www.linkedin.com/in/samueljamesperry</p>
+    <p>linkedin:https://www.linkedin.com/in/samueljamesperry/</p>
     <p>github:https://github.com/bladeyeight</p>
     </div>
 
