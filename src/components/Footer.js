2e2d2080abed6f73cbbcae6d6e66eb369755c0a0
bladeyeight <samueljperry1991@gmail.com>
2021-10-18 12:19:38 -0400

    added linked in and phone

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
 function Footer(props) {
-    return <h1>Footer</h1>;
+    return (
+    <div>
+    <h1>Footer</h1>
+    <p>phone:(804)3565005</p>
+    <p>linkedin:www.linkedin.com/in/samueljamesperry</p>
+    <p>github:https://github.com/bladeyeight</p>
+    </div>
+
+    )
   }
   
   export default Footer;
