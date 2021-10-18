a3b6590ca5835d7a4d9164b92a3d254518ad9aaf
bladeyeight <samueljperry1991@gmail.com>
2021-10-18 11:22:39 -0400

    added homepage message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return (
+    <div>
+    <h1>Home</h1>;
+    <h2>This is Sam Perry's Portfolio!</h2>
+    </div>
+    )
+
   }
   
   export default Home;
