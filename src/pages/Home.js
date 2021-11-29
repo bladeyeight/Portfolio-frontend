f87ca41c71333ff05a312840a54865fb7a8a8c18
bladeyeight <samueljperry1991@gmail.com>
2021-11-29 13:22:11 -0500

    removed padding on pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 function Home(props) {
     return (
     <div>
+      <h1></h1>
     <img id = "profpic" src = "https://media-exp1.licdn.com/dms/image/C4D03AQGII6sWXkmzxw/profile-displayphoto-shrink_800_800/0/1588787823310?e=1643846400&v=beta&t=7hqqZdsVi51ab55gNNVB61rSZhH6R1uZrK3guRHGsgw"></img>
     <ul id = "techlist"> Proficient In:
       <li>HTML</li>
