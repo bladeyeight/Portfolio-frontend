90577f08ad24949e12aa92ea4bae012f373c189a
bladeyeight <samueljperry1991@gmail.com>
2021-11-29 12:26:23 -0500

    changed some typos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 function Footer(props) {
     return (
     <div id = "pagefooter">
-    <p>Email:samueljperry1991@gmail.com</p>
-    <p>phone:(804)3565005</p>
-    <p>linkedin:https://www.linkedin.com/in/samueljamesperry/</p>
-    <p>github:https://github.com/bladeyeight</p>
+    <p>Email: samueljperry1991@gmail.com</p>
+    <p>phone: (804)356-5005</p>
+    <p>Linkedin: https://www.linkedin.com/in/samueljamesperry/</p>
+    <p>github: https://github.com/bladeyeight</p>
     </div>
 
     )
