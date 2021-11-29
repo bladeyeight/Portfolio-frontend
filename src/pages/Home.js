e9d08c33c41645ac11af9bb10933805e257a0520
bladeyeight <samueljperry1991@gmail.com>
2021-11-29 12:12:02 -0500

    added tech list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,20 @@ function Home(props) {
     <div>
     <h1>This is Sam Perry's Portfolio!</h1>
     <img id = "profpic" src = "https://media-exp1.licdn.com/dms/image/C4D03AQGII6sWXkmzxw/profile-displayphoto-shrink_800_800/0/1588787823310?e=1643846400&v=beta&t=7hqqZdsVi51ab55gNNVB61rSZhH6R1uZrK3guRHGsgw"></img>
+    <ul id = "techlist"> Proficient In:
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Express</li>
+      <li>Node.js</li>
+      <li>Ruby</li>
+      <li>Rails</li>
+      <li>SQL</li>
+      <li>C#</li>
+      <li>MongoDB</li>
+      <li>postgres</li>
+    </ul>
     </div>
     )
 
