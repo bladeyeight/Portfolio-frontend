1db8cfbb026ca9c8c2637ebc48a9e52b23566c07
bladeyeight <samueljperry1991@gmail.com>
2021-12-02 08:42:28 -0500

    think i got it this time

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ function Resume(props) {
       <div>
         <h1>Sam Perry's Resume</h1>
         <iframe
-        src = "/Users/Alice/samdocs/SamPerryResume/SamPerryResume.pdf" width = "100%" height = "500px">
+        src = "https://docs.google.com/document/d/e/2PACX-1vTkH1zxGxzwNH9GYLytaO9-mYK-6dYixNQs3TrCW9LcU-6VFhjuIyCwEXjr5oK1fXc18tvpRQRfVPKP/pub?embedded=true" width = "100%" height = "500px">
         </iframe>
       </div>
     )
