1841f5bedde53b8843516160e8459d8312bba68c
bladeyeight <samueljperry1991@gmail.com>
2021-12-02 08:27:28 -0500

    bugfixed resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ function Resume(props) {
       <div>
         <h1>Sam Perry's Resume</h1>
         <iframe
-        src = "https://www.linkedin.com/in/samueljamesperry/overlay/1635475744232/single-media-viewer?type=DOCUMENT&profileId=ACoAADCjw00B4QFKHP6Lh1Ap95HbI-yqY8VnhZ0&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BnkWZdvI8R%2BWQ1dtrFRTZnA%3D%3D" width = "100%" height = "500px">
+        src = "/Users/Alice/samdocs/SamPerryResume/SamPerryResume.pdf">
         </iframe>
       </div>
     )
