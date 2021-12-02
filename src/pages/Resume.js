aafbc9b7acf0dcde9c105b8929b2c21dda5d6f94
bladeyeight <samueljperry1991@gmail.com>
2021-12-02 09:22:25 -0500

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,7 +2,7 @@ function Resume(props) {
     return (
       <div>
         <iframe
-        src = "https://docs.google.com/document/d/e/2PACX-1vTkH1zxGxzwNH9GYLytaO9-mYK-6dYixNQs3TrCW9LcU-6VFhjuIyCwEXjr5oK1fXc18tvpRQRfVPKP/pub?embedded=true" width = "100%" height = "500px">
+        src = "https://docs.google.com/document/d/e/2PACX-1vTybBysMJwU3m9yJCzzADeMhQZHjY4Qz7eSOs5inhw69rJoBYdEBmWjdizqTh0kxKsuAJOAeRViYwk8/pub?embedded=true" width = "100%" height = "500px">
         </iframe>
       </div>
     )
