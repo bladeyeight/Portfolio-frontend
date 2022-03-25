89c0772f8c94d9b006f2822bd39ae6cde3ca1afe
Sam Perry <samueljperry1991@gmail.com>
2022-03-25 11:16:55 -0400

    fixed prof image again. Hopefully permanently

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ function Home(props) {
     return (
     <div>
       <h1></h1>
-    <img id = "profpic" src = "https://scontent-iad3-2.xx.fbcdn.net/v/t1.6435-9/47679602_2197014513677061_9191252824011309056_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=CzF7-i87V9IAX95T_8m&_nc_ht=scontent-iad3-2.xx&oh=00_AT8gf-BfUBZ7UblX7-VxTAKgjVUouRyVnHvLOl28tPCXFA&oe=61F45F4B"></img>
+    <img id = "profpic" src = "https://i.imgur.com/sCQSbzx.jpg"></img>
     <ul id = "techlist"> Proficient In:
       <li>HTML</li>
       <li>CSS</li>
