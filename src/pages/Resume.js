74bf7a8fb781dbca937a23b5da0150ab1dcbedfe
Sam Perry <samueljperry1991@gmail.com>
2022-04-25 11:43:28 -0400

    increased width of resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,7 +2,7 @@ function Resume(props) {
     return (
       <div id = "resume">
         <iframe
-        src = "https://docs.google.com/document/d/e/2PACX-1vTybBysMJwU3m9yJCzzADeMhQZHjY4Qz7eSOs5inhw69rJoBYdEBmWjdizqTh0kxKsuAJOAeRViYwk8/pub?embedded=true" height = "500px" width = "70%">
+        src = "https://docs.google.com/document/d/e/2PACX-1vTybBysMJwU3m9yJCzzADeMhQZHjY4Qz7eSOs5inhw69rJoBYdEBmWjdizqTh0kxKsuAJOAeRViYwk8/pub?embedded=true" height = "500px" width = "80%">
         </iframe>
       </div>
     )
