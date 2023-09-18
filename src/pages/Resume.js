f58e76eab59cc39e1a656ad1ec720cc7eb3c1474
bladeyeight <samueljperry1991@gmail.com>
2023-09-18 17:29:44 -0400

    possible solution

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,7 +2,7 @@ function Resume(props) {
     return (
       <div id = "resume">
         <iframe
-        src = "https://drive.google.com/file/d/1kjkanqA19vvWTlm5ysr9APtiGxjgnLJZ/pub" height = "500px" width = "80%">
+        src = "https://drive.google.com/file/d/1kjkanqA19vvWTlm5ysr9APtiGxjgnLJZ/preview" height = "500px" width = "80%">
         </iframe>
       </div>
     )
