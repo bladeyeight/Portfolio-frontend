f56324c57aac51cf20d1ee60c88cd5c87496ab4f
bladeyeight <samueljperry1991@gmail.com>
2023-09-18 16:28:28 -0400

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,7 +2,7 @@ function Resume(props) {
     return (
       <div id = "resume">
         <iframe
-        src = "https://docs.google.com/document/d/e/2PACX-1vTybBysMJwU3m9yJCzzADeMhQZHjY4Qz7eSOs5inhw69rJoBYdEBmWjdizqTh0kxKsuAJOAeRViYwk8/pub?embedded=true" height = "500px" width = "80%">
+        src = "https://drive.google.com/file/d/1kjkanqA19vvWTlm5ysr9APtiGxjgnLJZ/view?usp=sharing" height = "500px" width = "80%">
         </iframe>
       </div>
     )
