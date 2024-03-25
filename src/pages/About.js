cf5dff120b73bec94eb7b7a378006322e6646238
Sam Perry <samperry@Sams-MacBook-Air.local>
2024-03-25 09:58:15 -0400

    added header

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function About(props) {
  return (
    <div>
+     <h1>About Me</h1>
      <p>Sam Perry is a Washington DC based Software Engineer who has been working as a TEKSystems contractor for United Health Group and Optum after graduating from the College of William and Mary in 2013 and General Assembly in 2021.  Dedicated to leveraging skills in communication and full-stack development to connect people to solutions, whether they be coworkers or customers.</p>
      <h1>Certifications</h1>
      <iframe
